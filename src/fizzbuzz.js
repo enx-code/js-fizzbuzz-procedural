3c9f5ebf05eeb45df7e0748ecb0cccb59ac093a8
Enky <enkh6at@yahoo.com>
2022-07-11 20:42:36 +0300

    some changes

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,23 +2,19 @@ const answer = []
 
 // Write your code below this line
 for (let i = 1; i <= 15; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        answer.push("FizzBuzz")
-        // console.log("FizzBuzz")
-    } else if (i % 3 === 0) {
-        answer.push("Fizz")
-        // console.log("Fizz")
-    } else if (i % 5 === 0) {
-        answer.push("Buzz")
-        // console.log("Buzz")
-    } else {
-        answer.push(i)
-        // console.log(i)
-    }
+   if (i % 3 === 0 && i % 5 ===0) {
+   answer.push("FizzBuzz")
+   } else if (i % 3 === 0) {
+   answer.push("Fizz")
+   } else if (i % 5 ===0) {
+   answer.push("Fuzz")
+   } else {
+   answer.push(i)
+   }
 
 }
 
-
+console.log(answer)
 
 
 // Don't touch the code below this line, we'll cover it later
